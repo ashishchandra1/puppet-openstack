facf9b51c1657c87827c7cb8f8f774f401e78915
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-03-28 12:27:57 +0400

    Minor changes

--- openstack-glance/manifests/init.pp
+++
@@ -1,4 +0,0 @@
-class openstack-glance {
-    include openstack-glance::keystone
-    include openstack-glance::glance
-}
--- openstack-glance/templates/glance-init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Register service and endpoints for keystone in keystone.
-glance_host="<%= @GLANCE_HOST %>"
-admin_port="<%= @GLANCE_ADMIN_PORT %>"
-admin_token="<%= @ADMIN_TOKEN %>"
-export SERVICE_ENDPOINT="http://${glance_host}:${admin_port}/v2.0/"
-export SERVICE_TOKEN="${admin_token}"
-# Create the database.
-su -s /bin/sh -c "glance-manage db_sync" glance
