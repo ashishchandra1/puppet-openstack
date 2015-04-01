1f4850e8f11de978e8a019b8d5d0cd55704bd99d
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-04-01 16:51:58 +0400

    Restructed glance module

--- modules/openstack-glance/manifests/init.pp
+++ modules/openstack-glance/manifests/init.pp
@@ -1,9 +1,19 @@
-#class openstack-glance {
-#    include openstack-glance::keystone
-#    include openstack-glance::glance
-#}
-
 class openstack-glance {
-   class {'keystone':} -> 
-   class {'glance':}
+
+   notify {"Openstack keystone runs on Primary node": } ->
+   case $hostname {
+         controller1: {
+                class {'keystone':} -> 
+                class {'glance-installation':} ->
+                class {'glance-db-sync':} ->
+                class {'glance-operations':}
+             }
+
+          controller2,controller3: {
+                class {'keystone':} ->
+                class {'glance-installation':} ->
+                class {'glance-operations':}                    
+              }
+   }
+   
 }
