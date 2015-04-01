c33aacf56aa8eebebd730985af7c12b2082c8880
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-04-01 16:33:26 +0400

    Modified keystone templates to identify duplicate user and service

--- modules/openstack-cinder/templates/keystone-cinder-operations.sh
+++ modules/openstack-cinder/templates/keystone-cinder-operations.sh
@@ -19,17 +19,89 @@ export OS_AUTH_URL=http://${keystone_host}:${admin_port}/v2.0
 
 
 # Creat the cinder user.
-keystone user-create --name=cinder --pass="$admin_user_pass"
 
 
-# Add the admin role to cinder user
-keystone  user-role-add --user cinder --tenant service --role admin
+get_user_id () {
+        keystone user-list |
+                awk -F'|' '{print $2,$3,$4}' |
+                awk -vuser_name="$1" '
+                        $2 == user_name  {print $1}
+                '
+}
 
-# Create a cinder service
-keystone service-create --name cinder --type volume  --description "OpenStack Block Storage"
+user_id=$(get_user_id cinder)
+
+if [ "$user_id" ]; then
+        echo "Found existing user id: $user_id"
+else
+        # Creat the user
+        keystone user-create \
+                --name=cinder \
+                --pass="$admin_user_pass" > /dev/null
+        # Add the admin role to glance user
+        keystone  user-role-add --user cinder --tenant service --role admin
+        user_id=$(get_user_id cinder)
+
+        if [ "$user_id" ]; then
+                echo "Created new User: $user_id"
+        else
+                echo "ERROR: Failed to create User."
+                exit 1
+        fi
+fi
+
+#Create a Cinder service
+get_service_id () {
+        keystone service-list |
+                awk -F'|' '{print $2,$3,$4}' |
+                awk -vservice_name="$1" -vservice_type="$2" '
+                        $2 == service_name && $3 == service_type {print $1}
+                '
+}
+
+service_id=$(get_service_id cinder volume)
+
+if [ "$service_id" ]; then
+        echo "Found existing service id: $service_id"
+else
+        # Creat the service and endpoint.
+        keystone service-create \
+                --name=cinder \
+                --type=volume \
+                --description="OpenStack Block Storage" > /dev/null
+
+        service_id=$(get_service_id cinder volume)
+
+        if [ "$service_id" ]; then
+                echo "Created new service id: $service_id"
+        else
+                echo "ERROR: Failed to create service."
+                exit 1
+        fi
+fi
+
 
 #Create a cinderv2 service
-keystone service-create --name cinderv2 --type volumev2 --description "OpenStack Block Storage"
+service_id=$(get_service_id cinderv2 volumev2)
+
+if [ "$service_id" ]; then
+        echo "Found existing service id: $service_id"
+else
+        # Creat the service and endpoint.
+        keystone service-create \
+                --name=cinderv2 \
+                --type=volumev2 \
+                --description="OpenStack Block Storage" > /dev/null
+
+        service_id=$(get_service_id cinderv2 volumev2)
+
+        if [ "$service_id" ]; then
+                echo "Created new service id: $service_id"
+        else
+                echo "ERROR: Failed to create service."
+                exit 1
+        fi
+fi
 
 # Create the Block Service API endpoints
 
--- modules/openstack-glance/templates/keystone-glance-operations.sh
+++ modules/openstack-glance/templates/keystone-glance-operations.sh
@@ -18,15 +18,67 @@ export OS_PASSWORD=${admin_user_pass}
 export OS_AUTH_URL=http://${keystone_host}:${admin_port}/v2.0
 
 
-# Creat the glance user.
-keystone user-create --name=glance --pass="$admin_user_pass"
+get_user_id () {
+        keystone user-list |
+                awk -F'|' '{print $2,$3,$4}' |
+                awk -vuser_name="$1" '
+                        $2 == user_name  {print $1}
+                '
+}
+
+user_id=$(get_user_id glance)
+
+if [ "$user_id" ]; then
+        echo "Found existing user id: $user_id"
+else
+        # Creat the user
+        keystone user-create \
+                --name=glance \
+                --pass="$admin_user_pass" > /dev/null
+        
+        keystone  user-role-add --user glance --tenant service --role admin
+        user_id=$(get_user_id $user_name)
+
+        if [ "$user_id" ]; then
+                echo "Created new User: $user_id"
+        else
+                echo "ERROR: Failed to create User."
+                exit 1
+        fi
+fi
 
 
-# Add the admin role to glance user
-keystone  user-role-add --user glance --tenant service --role admin
 
-# Create a glance service
-keystone service-create --name glance --type image  --description "OpenStack Image Service"
+get_service_id () {
+        keystone service-list |
+                awk -F'|' '{print $2,$3,$4}' |
+                awk -vservice_name="$1" -vservice_type="$2" '
+                        $2 == service_name && $3 == service_type {print $1}
+                '
+}
+
+
+service_id=$(get_service_id glance image)
+
+if [ "$service_id" ]; then
+        echo "Found existing service id: $service_id"
+else
+        # Creat the service and endpoint.
+        keystone service-create \
+                --name=glance \
+                --type=image \
+                --description="Openstack Image Service" > /dev/null
+
+        service_id=$(get_service_id $service_name $service_type)
+
+        if [ "$service_id" ]; then
+                echo "Created new service id: $service_id"
+        else
+                echo "ERROR: Failed to create service."
+                exit 1
+        fi
+fi
+
 
 # Create the Image Service API endpoints
 keystone endpoint-create  --service-id $(keystone  service-list | awk '/ image / {print $2}') --publicurl http://"$keystone_host":9292 --internalurl http://"$keystone_host":9292 --adminurl http://"$keystone_host":9292 --region "$region"
