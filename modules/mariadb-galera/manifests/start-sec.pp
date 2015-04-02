7380591a9a4a2c5f8098cad9c67e6e1242daaff2
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-04-02 10:45:55 +0400

    Fixed installation of maria on secondary nodes

--- modules/mariadb-galera/manifests/start-sec.pp
+++ modules/mariadb-galera/manifests/start-sec.pp
@@ -3,8 +3,8 @@ class mariadb-galera::start-sec {
 
    notify {"Initialized cluster on primary node":} ->
     exec {"Initialized cluster on primary node":
-    path => ["/usr/bin/","/usr/sbin/","/bin"],
-    command => "/etc/init.d/mysql start",
+    #path => ["/usr/bin/","/usr/sbin/","/bin"],
+    command => "service mysql start",
     user => 'root',
   }
 }
