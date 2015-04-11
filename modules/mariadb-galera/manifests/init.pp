a21b90fc71f8d2836a78d80a25dfe85f20aedf4c
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-04-11 14:25:49 +0400

    Added neutron database

--- modules/mariadb-galera/manifests/init.pp
+++ modules/mariadb-galera/manifests/init.pp
@@ -15,6 +15,7 @@ class mariadb-galera {
                class {'mariadb-galera::glance':}->
                class {'mariadb-galera::cinder':}->
                class {'mariadb-galera::nova-controller':}
+               class {'mariadb-galera::neutron':}
   
        }
      }
