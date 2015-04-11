2d26112c6cbe5cab6f8ccde942987b9a8771d0ca
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-04-11 11:30:31 +0400

    Renamed a few files

--- modules/openstack-nova/manifests/init.pp
+++ modules/openstack-nova/manifests/init.pp
@@ -2,15 +2,19 @@ class openstack-nova {
    case $hostname {
          controller1: {
                 class {'keystone':} ->
-                class {'nova-installation':} ->
+                class {'nova-controller-installation':} ->
                 class {'nova-db-sync':} ->
-                class {'nova-operations':}
+                class {'nova-controller-operations':}
              }
 
           controller2,controller3: {
-                class {'nova-installation':} ->
-                class {'nova-operations':}
+                class {'nova-controller-installation':} ->
+                class {'nova-controller-operations':}
               }
+
+          compute1,compute2: {
+                class {'nova-compute-installation':} ->
+                class {'nova-compute-operations':}
    }
 }
 
