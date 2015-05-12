fb45e6dd3d602f4493688bc070a566b948fed265
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-05-12 17:01:49 +0400

    Update keystone-nova-operations.sh

--- modules/openstack-nova/templates/nova-controller/keystone-nova-operations.sh
+++ modules/openstack-nova/templates/nova-controller/keystone-nova-operations.sh
@@ -18,13 +18,8 @@ export OS_PASSWORD=${admin_user_pass}
 export OS_AUTH_URL=http://${keystone_host}:${admin_port}/v2.0
 
 # Creat the nova user.
-
 get_user_id () {
-        keystone user-list |
-                awk -F'|' '{print $2,$3,$4}' |
-                awk -vuser_name="$1" '
-                        $2 == user_name  {print $1}
-                '
+        keystone user-list | awk -F'|' '{print $2,$3,$4}' | awk -vuser_name="$1" ' $2 == user_name  {print $1}'
 }
 
 user_id=$(get_user_id nova)
@@ -37,7 +32,7 @@ else
 
         # Add the admin role to nova user
         keystone user-role-add --user nova --tenant service --role admin
-
+        
         user_id=$(get_user_id nova)
 
         if [ "$user_id" ]; then
@@ -50,22 +45,17 @@ fi
 
 #Create a Nova service
 get_service_id () {
-        keystone service-list |
-                awk -F'|' '{print $2,$3,$4}' |
-                awk -vservice_name="$1" -vservice_type="$2" '
-                        $2 == service_name && $3 == service_type {print $1}
-                '
+        keystone service-list |  awk -F'|' '{print $2,$3,$4}' | awk -vservice_name="$1" -vservice_type="$2" 
+                '$2 == service_name && $3 == service_type {print $1}'
 }
+
 service_id=$(get_service_id nova compute )
 
 if [ "$service_id" ]; then
         echo "Found existing service id: $service_id"
 else
         # Creat the service and endpoint.
-        keystone service-create \
-                --name=nova \
-                --type=compute \
-                --description="OpenStack Compute" > /dev/null
+        keystone service-create --name=nova --type=compute --description="OpenStack Compute" > /dev/null
 
         service_id=$(get_service_id nova compute)
 
@@ -77,8 +67,19 @@ else
         fi
 fi
 
+get_keystone_endpoint () {
+        keystone endpoint-list | awk -F'|' '{print $7}' | awk -vservice_id="$1" '$1 == service_id  {print $1}'
+}
 
-# Create the Compute Service API endpoints
+endpoint_id=$(get_keystone_endpoint $service_id )
 
-keystone endpoint-create  --service-id $(keystone service-list | awk '/ compute / {print $2}') --publicurl http://"$keystone_host":8774/v2/%\(tenant_id\)s --internalurl http://"$keystone_host":8774/v2/%\(tenant_id\)s --adminurl http://"$keystone_host":8774/v2/%\(tenant_id\)s --region "$region"
+if [ "$endpoint_id" ]; then
+        echo "Found existing endpoint: $endpoint_id"
+else
+   # Create the Compute Service API endpoints
+   keystone endpoint-create  --service-id $(keystone service-list | awk '/ compute / {print $2}') \
+            --publicurl http://"$keystone_host":8774/v2/%\(tenant_id\)s \
+            --internalurl http://"$keystone_host":8774/v2/%\(tenant_id\)s \ 
+            --adminurl http://"$keystone_host":8774/v2/%\(tenant_id\)s \
+            --region "$region" > /dev/null
 
