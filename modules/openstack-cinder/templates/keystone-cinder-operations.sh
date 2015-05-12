58fc805d2aeb6a57afd1e89a40d3450847ca7483
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-05-12 17:58:36 +0400

    Fixed keystone endpoint issue in cinder, glance, and neutron

--- modules/openstack-cinder/templates/keystone-cinder-operations.sh
+++ modules/openstack-cinder/templates/keystone-cinder-operations.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Create cinder user and service.
+# Create cinder user and service and endpoint.
 
 keystone_host="<%=  @KEYSTONE_HOST %>"
 admin_port="<%= @KEYSTONE_ADMIN_PORT %>"
@@ -18,15 +18,9 @@ export OS_PASSWORD=${admin_user_pass}
 export OS_AUTH_URL=http://${keystone_host}:${admin_port}/v2.0
 
 
-# Creat the cinder user.
-
-
+# Create the Cinder user.
 get_user_id () {
-        keystone user-list |
-                awk -F'|' '{print $2,$3,$4}' |
-                awk -vuser_name="$1" '
-                        $2 == user_name  {print $1}
-                '
+        keystone user-list | awk -F'|' '{print $2,$3,$4}' | awk -vuser_name="$1" ' $2 == user_name  {print $1} '
 }
 
 user_id=$(get_user_id cinder)
@@ -34,11 +28,9 @@ user_id=$(get_user_id cinder)
 if [ "$user_id" ]; then
         echo "Found existing user id: $user_id"
 else
-        # Creat the user
-        keystone user-create \
-                --name=cinder \
-                --pass="$admin_user_pass" > /dev/null
-        # Add the admin role to glance user
+        # Create the Cinder user
+        keystone user-create --name=cinder --pass="$admin_user_pass" > /dev/null
+        # Add the admin role to Cinder user
         keystone  user-role-add --user cinder --tenant service --role admin
         user_id=$(get_user_id cinder)
 
@@ -52,59 +44,71 @@ fi
 
 #Create a Cinder service
 get_service_id () {
-        keystone service-list |
-                awk -F'|' '{print $2,$3,$4}' |
-                awk -vservice_name="$1" -vservice_type="$2" '
-                        $2 == service_name && $3 == service_type {print $1}
-                '
+        keystone service-list | awk -F'|' '{print $2,$3,$4}' | awk -vservice_name="$1" -vservice_type="$2" ' $2 == service_name && $3 == service_type {print $1} '
 }
 
-service_id=$(get_service_id cinder volume)
+service_id_v1=$(get_service_id cinder volume)
 
-if [ "$service_id" ]; then
-        echo "Found existing service id: $service_id"
+if [ "$service_id_v1" ]; then
+        echo "Found existing service id: $service_id_v1"
 else
-        # Creat the service and endpoint.
-        keystone service-create \
-                --name=cinder \
-                --type=volume \
-                --description="OpenStack Block Storage" > /dev/null
+        # Create the Service
+        keystone service-create --name=cinder --type=volume --description="OpenStack Block Storage" > /dev/null
 
-        service_id=$(get_service_id cinder volume)
+        service_id_v1=$(get_service_id cinder volume)
 
-        if [ "$service_id" ]; then
-                echo "Created new service id: $service_id"
+        if [ "$service_id_v1" ]; then
+                echo "Created new service id: $service_id_v1"
         else
                 echo "ERROR: Failed to create service."
                 exit 1
         fi
 fi
 
+#Create a Cinderv2 service
+service_id_v2=$(get_service_id cinderv2 volumev2)
 
-#Create a cinderv2 service
-service_id=$(get_service_id cinderv2 volumev2)
-
-if [ "$service_id" ]; then
-        echo "Found existing service id: $service_id"
+if [ "$service_id_v2" ]; then
+        echo "Found existing service id: $service_id_v2"
 else
-        # Creat the service and endpoint.
-        keystone service-create \
-                --name=cinderv2 \
-                --type=volumev2 \
-                --description="OpenStack Block Storage" > /dev/null
+        # Creat the service
+        keystone service-create --name=cinderv2 --type=volumev2 --description="OpenStack Block Storage" > /dev/null
 
-        service_id=$(get_service_id cinderv2 volumev2)
+        service_id_v2=$(get_service_id cinderv2 volumev2)
 
-        if [ "$service_id" ]; then
-                echo "Created new service id: $service_id"
+        if [ "$service_id_v2" ]; then
+                echo "Created new service id: $service_id_v2"
         else
                 echo "ERROR: Failed to create service."
                 exit 1
         fi
 fi
 
-# Create the Block Service API endpoints
+# Create the Cinder Service API endpoints
+get_keystone_endpoint () {
+        keystone endpoint-list | awk -F'|' '{print $7}' | awk -vservice_id="$1" '$1 == service_id  {print $1}'
+}
 
-keystone endpoint-create  --service-id $(keystone service-list | awk '/ volumev2 / {print $2}') --publicurl http://"$keystone_host":8776/v2/%\(tenant_id\)s --internalurl http://"$keystone_host":8776/v2/%\(tenant_id\)s --adminurl http://"$keystone_host":8776/v2/%\(tenant_id\)s --region "$region"
+endpoint_id_v1=$(get_keystone_endpoint $service_id_v1 )
+endpoint_id_v2=$(get_keystone_endpoint $service_id_v2 )
 
-keystone endpoint-create --service-id $(keystone service-list | awk '/ volume / {print $2}') --publicurl http://"$keystone_host":8776/v1/%\(tenant_id\)s --internalurl http://"$keystone_host":8776/v1/%\(tenant_id\)s --adminurl http://"$keystone_host":8776/v1/%\(tenant_id\)s --region "$region"
+if [ "$endpoint_id_v1" ]; then
+        echo "Found existing endpoint: $endpoint_id_v1"
+else
+        keystone endpoint-create  --service-id $(keystone service-list | awk '/ volumev2 / {print $2}') \
+                --publicurl http://"$keystone_host":8776/v1/%\(tenant_id\)s \
+                --internalurl http://"$keystone_host":8776/v1/%\(tenant_id\)s \
+                --adminurl http://"$keystone_host":8776/v1/%\(tenant_id\)s \
+                --region "$region" > /dev/null
+fi
+
+if [ "$endpoint_id_v2" ]; then
+        echo "Found existing endpoint: $endpoint_id_v2"
+else
+        keystone endpoint-create --service-id $(keystone service-list | awk '/ volume / {print $2}') \
+                --publicurl http://"$keystone_host":8776/v2/%\(tenant_id\)s \
+                --internalurl http://"$keystone_host":8776/v2/%\(tenant_id\)s \
+                --adminurl http://"$keystone_host":8776/v2/%\(tenant_id\)s \
+                --region "$region" > /dev/null
+
+fi
--- modules/openstack-glance/templates/keystone-glance-operations.sh
+++ modules/openstack-glance/templates/keystone-glance-operations.sh
@@ -17,13 +17,9 @@ export OS_USERNAME=${admin_user}
 export OS_PASSWORD=${admin_user_pass}
 export OS_AUTH_URL=http://${keystone_host}:${admin_port}/v2.0
 
-
+# Create the Glance User
 get_user_id () {
-        keystone user-list |
-                awk -F'|' '{print $2,$3,$4}' |
-                awk -vuser_name="$1" '
-                        $2 == user_name  {print $1}
-                '
+        keystone user-list | awk -F'|' '{print $2,$3,$4}' | awk -vuser_name="$1" ' $2 == user_name  {print $1} '
 }
 
 user_id=$(get_user_id glance)
@@ -31,11 +27,9 @@ user_id=$(get_user_id glance)
 if [ "$user_id" ]; then
         echo "Found existing user id: $user_id"
 else
-        # Creat the user
-        keystone user-create \
-                --name=glance \
-                --pass="$admin_user_pass" > /dev/null
-        
+        # Create the user
+        keystone user-create --name=glance --pass="$admin_user_pass" > /dev/null
+        # Add the admin role to glance user
         keystone  user-role-add --user glance --tenant service --role admin
         user_id=$(get_user_id $user_name)
 
@@ -48,26 +42,18 @@ else
 fi
 
 
-
+#Create a Glance service
 get_service_id () {
-        keystone service-list |
-                awk -F'|' '{print $2,$3,$4}' |
-                awk -vservice_name="$1" -vservice_type="$2" '
-                        $2 == service_name && $3 == service_type {print $1}
-                '
+        keystone service-list | awk -F'|' '{print $2,$3,$4}' | awk -vservice_name="$1" -vservice_type="$2" ' $2 == service_name && $3 == service_type {print $1} '
 }
-
 
 service_id=$(get_service_id glance image)
 
 if [ "$service_id" ]; then
         echo "Found existing service id: $service_id"
 else
-        # Creat the service and endpoint.
-        keystone service-create \
-                --name=glance \
-                --type=image \
-                --description="Openstack Image Service" > /dev/null
+        # Creat the service 
+        keystone service-create --name=glance --type=image --description="Openstack Image Service" > /dev/null
 
         service_id=$(get_service_id $service_name $service_type)
 
@@ -79,6 +65,19 @@ else
         fi
 fi
 
-
 # Create the Image Service API endpoints
-keystone endpoint-create  --service-id $(keystone  service-list | awk '/ image / {print $2}') --publicurl http://"$keystone_host":9292 --internalurl http://"$keystone_host":9292 --adminurl http://"$keystone_host":9292 --region "$region"
+get_keystone_endpoint () {
+        keystone endpoint-list | awk -F'|' '{print $7}' | awk -vservice_id="$1" '$1 == service_id  {print $1}'
+}
+
+endpoint_id=$(get_keystone_endpoint $service_id )
+
+if [ "$endpoint_id" ]; then
+        echo "Found existing endpoint: $endpoint_id"
+else
+    keystone endpoint-create  --service-id $(keystone  service-list | awk '/ image / {print $2}') \
+             --publicurl http://"$keystone_host":9292 
+             --internalurl http://"$keystone_host":9292 
+             --adminurl http://"$keystone_host":9292 
+             --region "$region" > /dev/null
+fi             
