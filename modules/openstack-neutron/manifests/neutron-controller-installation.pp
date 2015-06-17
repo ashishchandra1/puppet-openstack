0b43bf80eb0d16455d349525a36d7cf72d1b91a0
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-06-17 23:34:42 +0400

    Minor refactoring in code

--- modules/openstack-neutron/manifests/neutron-controller-installation.pp
+++ modules/openstack-neutron/manifests/neutron-controller-installation.pp
@@ -1,56 +1,41 @@
 class openstack-neutron::neutron-controller-installation {
 
-     $NEUTRON_DBPASS = '@dmin123'
-     $CONTROLLER_HNAME = "controller"
-     $NEUTRON_ADMIN_PORT = '9696'
-     $ADMIN_USER = 'neutron'
-     $ADMIN_PASSWORD = '@dmin123'
-     $RABBIT_PASSWORD = '@dmin123' 
+    $NEUTRON_DBPASS = '@dmin123'
+    $CONTROLLER_HNAME = "controller"
+    $NEUTRON_ADMIN_PORT = '9696'
+    $ADMIN_USER = 'neutron'
+    $ADMIN_PASSWORD = '@dmin123'
+    $RABBIT_PASSWORD = '@dmin123' 
 
-     $KEYSTONE_HOST = "controller"
-     $KEYSTONE_ADMIN_PORT = '35357'
-     $ADMIN_TOKEN = '@dmin123'
-    
-     $VERBOSE = "True"
-     $RABBIT_HOSTS = "controller1:5672,controller2:5672,controller3:5672"
-     $MY_IP = "10.0.129.20"
-     $REGION = "regionOne"
-     $NOVA_ADMIN_USERNAME  = "nova"
-     $NOVA_ADMIN_PASSWORD  = "@dmin123"
+    $KEYSTONE_HOST = "controller"
+    $KEYSTONE_ADMIN_PORT = '35357'
+    $ADMIN_TOKEN = '@dmin123'
+    $VERBOSE = "True"
+    $RABBIT_HOSTS = "controller1:5672,controller2:5672,controller3:5672"
+    $MY_IP = "10.0.129.20"
+    $REGION = "regionOne"
+    $NOVA_ADMIN_USERNAME  = "nova"
+    $NOVA_ADMIN_PASSWORD  = "@dmin123"
      
-     $packages = [ 
-                 "openstack-neutron",
-                 "openstack-neutron-ml2",
-                 "python-neutronclient",
-                 "which"
+    $packages = [ 
+        "openstack-neutron",
+        "openstack-neutron-ml2",
+        "python-neutronclient",
+        "which"
     ]
     
-     notify {"Installing Openstack Neutron on Controller Nodes":} ->
-     package {
-          $packages: 
-          ensure =>installed,
-          before => File['/etc/neutron/neutron.conf'],
+    notify {"Installing Openstack Neutron on Controller Nodes":} ->
+    package {
+        $packages: 
+        ensure =>installed,
+        before => File['/etc/neutron/neutron.conf'],
     } -> 
 
-   notify {"CREATING neutron.conf FILE":} ->
-   file { "/etc/neutron/neutron.conf":
-       ensure  => file,
-       owner  => root,
-       group  => neutron,
-       content => template('openstack-neutron/neutron-controller/neutron.conf.erb'),
-   } ->
-
-   notify {"Putting Service ID in neutron.conf file":} ->
-   file {"/tmp/get-nova-admin-tenant-id.sh":
-        ensure =>file,
-        owner =>root,
-        content => template('openstack-neutron/neutron-controller/get-nova-tenant-service-id.sh'),
-        mode => 755,
-   } -> 
-
-   notify {"Getting nova admin tenant id":} ->
-   exec {"Nova admin tenant id":
-             command => "bash /tmp/get-nova-admin-tenant-id.sh",
-             user => 'root',
-       }
+    notify {"CREATING neutron.conf FILE":} ->
+    file { "/etc/neutron/neutron.conf":
+        ensure  => file,
+        owner  => root,
+        group  => neutron,
+        content => template('openstack-neutron/neutron-controller/neutron.conf.erb'),
+    }
 }
--- modules/openstack-neutron/manifests/neutron-ml2-config.pp
+++ modules/openstack-neutron/manifests/neutron-ml2-config.pp
@@ -1,28 +1,18 @@
 class openstack-neutron::neutron-ml2-config {
 
-     $KEYSTONE_HOST = "controller"
-     $KEYSTONE_ADMIN_PORT = '35357'
-     $ADMIN_TOKEN = '@dmin123'
-     
-     $admin_tenant = "admin"
-     $admin_user =  "admin"
-     $admin_user_pass = "@dmin123"
-     $region = "regionOne"
-
-     
-     notify {"CREATING ml2_conf.ini FILE":} ->
-        file { "/etc/neutron/plugins/ml2/ml2_conf.ini":
+    notify {"CREATING ml2_conf.ini FILE":} ->
+      file { "/etc/neutron/plugins/ml2/ml2_conf.ini":
         ensure  => file,
         owner  => root,
         group  => neutron,
         content => template('openstack-neutron/neutron-controller/ml2.conf.erb') 
-     } ->
+    } ->
 
-     notify {"Creating Symbolic link":} ->
+    notify {"Creating Symbolic link":} ->
 
-     file {'/etc/neutron/plugin.ini':
-         ensure => link,
-         target => '/etc/neutron/plugins/ml2/ml2_conf.ini',  
-     }
+    file {'/etc/neutron/plugin.ini':
+        ensure => link,
+        target => '/etc/neutron/plugins/ml2/ml2_conf.ini',  
+    }
 }
 
