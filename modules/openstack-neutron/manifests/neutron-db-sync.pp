570c111b2788ac883282db64c77859a9fa7381c9
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-06-18 16:52:11 +0400

    Update neutron-db-sync.pp

--- modules/openstack-neutron/manifests/neutron-db-sync.pp
+++ modules/openstack-neutron/manifests/neutron-db-sync.pp
@@ -9,7 +9,7 @@ class openstack-neutron::neutron-db-sync {
 
   notify {"Syncing DB For Neutron":} ->
   exec {"Nova DB Syncing":
-     command => "su -s /bin/sh -c 'neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade juno' neutron",
+     command => "su -s /bin/sh -c 'neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head' neutron",
      user => 'root',
   } 
 
