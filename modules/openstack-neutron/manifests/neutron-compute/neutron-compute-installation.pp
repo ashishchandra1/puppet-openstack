dca9af4efb92811e700435ab449898ba44fbe1f6
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-06-18 17:32:47 +0400

    Added code for neutron on compute node

--- modules/openstack-neutron/manifests/neutron-compute/neutron-compute-installation.pp
+++ modules/openstack-neutron/manifests/neutron-compute/neutron-compute-installation.pp
@@ -1,33 +1,34 @@
 class openstack-neutron::neutron-compute::neutron-compute-installation {
 
-     $CONTROLLER_HNAME = "controller"
-     $ADMIN_PASSWORD = '@dmin123'
-     $RABBIT_PASSWORD = '@dmin123'
+    $CONTROLLER_HNAME = "controller"
+    $ADMIN_PASSWORD = '@dmin123'
+    $RABBIT_PASSWORD = '@dmin123'
  
-     $VERBOSE = "True"
-     $RABBIT_HOSTS = "controller1:5672,controller2:5672,controller3:5672"
+    $VERBOSE = "True"
+    $RABBIT_HOSTS = "controller1:5672,controller2:5672,controller3:5672"
     
-     $packages = [
-              "openstack-neutron-ml2",
-              "openstack-neutron-openvswitch"
-     ]
+    $packages = [
+        "openstack-neutron",
+        "openstack-neutron-ml2",
+        "openstack-neutron-openvswitch"
+    ]
   
-     notify {"Configure kernel networking parameter on Compute Node":} ->
-     file { "/etc/sysctl.conf":
+    notify {"Configure kernel networking parameter on Compute Node":} ->
+    file { "/etc/sysctl.conf":
         ensure  => file,
         owner  => root,
         content => template('openstack-neutron/neutron-compute/sysctl.conf.erb')
-      } ->
+    } ->
      
-     exec {"Configure kernel networking on Compute node":
-          command =>"sysctl -p",
-          user => 'root',
-     } -> 
+    exec {"Configure kernel networking on Compute node":
+        command =>"sysctl -p",
+        user => 'root',
+    } -> 
 
-     notify {"Installing Openstack Neutron on Compute Nodes":} ->
-     package {
-          $packages: 
-          ensure =>installed,
+    notify {"Installing Openstack Neutron on Compute Nodes":} ->
+    package {
+        $packages: 
+        ensure =>installed,
     } -> 
 
     notify {"CREATING neutron.conf file on Compute Node":} ->
@@ -37,5 +38,4 @@ class openstack-neutron::neutron-compute::neutron-compute-installation {
         group  => neutron,
         content => template('openstack-neutron/neutron-compute/neutron.conf.erb'),
    }
-
 }
--- modules/openstack-neutron/manifests/neutron-compute/neutron-compute-operations.pp
+++ modules/openstack-neutron/manifests/neutron-compute/neutron-compute-operations.pp
@@ -5,37 +5,37 @@ class openstack-neutron::neutron-compute::neutron-compute-operations {
     file {'/etc/neutron/plugin.ini':
         ensure => link,
         target => '/etc/neutron/plugins/ml2/ml2_conf.ini',  
-      } ->
+    } ->
 
     notify{"Copy files on Compute Node to resolve packaging bug":} ->
     exec {"Copy files on Compute Node":
-         command =>'cp /usr/lib/systemd/system/neutron-openvswitch-agent.service \
-                /usr/lib/systemd/system/neutron-openvswitch-agent.service.orig',
-         user => 'root',
-      } ->
+        command =>'cp /usr/lib/systemd/system/neutron-openvswitch-agent.service \
+                   /usr/lib/systemd/system/neutron-openvswitch-agent.service.orig',
+        user => 'root',
+    } ->
    
     notify{"Find and Replace to resolve packaging bug":}->
     exec {"Find and Replace on Compute Node":
-          command =>"sed -i 's,plugins/openvswitch/ovs_neutron_plugin.ini,plugin.ini,g' \
+        command =>"sed -i 's,plugins/openvswitch/ovs_neutron_plugin.ini,plugin.ini,g' \
                     /usr/lib/systemd/system/neutron-openvswitch-agent.service",
-          user =>'root',
-     } ->
+        user =>'root',
+    } ->
 
     notify {"Restart Compute Service":} ->
     exec {"Restarting Compute Service":
-          command => 'systemctl  restart openstack-nova-compute.service',
-          user => 'root',
-     } ->
+        command => 'systemctl restart openstack-nova-compute.service',
+        user => 'root',
+    } ->
 
     notify {"Enable OVS agent on Compute Node":} ->
     exec {"Enabling OVS agent":
-          command => 'systemctl enable neutron-openvswitch-agent.service',
-          user => 'root',
-     } ->
+        command => 'systemctl enable neutron-openvswitch-agent.service',
+        user => 'root',
+    } ->
 
     notify {"Start OVS agent on Compute Node":} ->
     exec {"Starting OVS agent":
-          command => 'systemctl start neutron-openvswitch-agent.service',
-          user => 'root',
-     } 
+        command => 'systemctl start neutron-openvswitch-agent.service',
+        user => 'root',
+    } 
 }
