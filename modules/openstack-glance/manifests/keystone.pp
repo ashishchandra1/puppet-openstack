5230d6451b8b5a109cf26bbbe6ef0a2febd508ce
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-06-24 18:47:00 +0400

    refactored glance code

--- modules/openstack-glance/manifests/keystone.pp
+++ modules/openstack-glance/manifests/keystone.pp
@@ -1,26 +1,14 @@
-class openstack-glance::keystone {
+class openstack-glance::keystone inherits openstack-glance::params {
 
-     $GLANCE_DBPASS = '@dmin123'
-     $CONTROLLER_HNAME = "controller" 
-     $GLANCE_ADMIN_PORT = '9292'
+    notify {"Copy file for Keystone operations":} ->
+    file { '/tmp/keystone-glance-operations.sh':
+        content => template('openstack-glance/keystone-glance-operations.sh'),
+        mode    => 0755,
+    } ->  
 
-     $KEYSTONE_HOST = "controller"
-     $KEYSTONE_ADMIN_PORT = '35357'
-     $ADMIN_TOKEN = '@dmin123'
-     
-     $admin_tenant = "admin"
-     $admin_user =  "admin"
-     $admin_user_pass = "@dmin123"
-     $region = "regionOne"
-     notify {"Copy file for Keystone operations":} ->
-     file { '/tmp/keystone-glance-operations.sh':
-            content => template('openstack-glance/keystone-glance-operations.sh'),
-            mode    => 0755,
-          } ->  
-
-      notify {"Keystone operations for Glance":} ->
-      exec {"keystone operations":
-            command => "bash /tmp/keystone-glance-operations.sh",
-            user => 'root',
-      } 
+    notify {"Keystone operations for Glance":} ->
+    exec {"keystone operations":
+        command => "bash /tmp/keystone-glance-operations.sh",
+        user => 'root',
+    } 
 }
