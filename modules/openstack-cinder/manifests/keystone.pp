e71fc96c107ed6d53b3f6cdc607a4d9158b2467f
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-06-24 18:56:17 +0400

    refactored opensatck cinder

--- modules/openstack-cinder/manifests/keystone.pp
+++ modules/openstack-cinder/manifests/keystone.pp
@@ -1,25 +1,14 @@
-class openstack-cinder::keystone {
+class openstack-cinder::keystone inherits openstack-cinder::params {
 
-     $CINDER_DBPASS = '@dmin123'
-     $CONTROLLER_HNAME = "controller" 
+    notify {"Copy file for Keystone Cinder operations":} ->
+    file { '/tmp/keystone-cinder-operations.sh':
+        content => template('openstack-cinder/keystone-cinder-operations.sh'),
+        mode    => 0755,
+    } ->  
 
-     $KEYSTONE_HOST = "controller"
-     $KEYSTONE_ADMIN_PORT = '35357'
-     $ADMIN_TOKEN = '@dmin123'
-     
-     $admin_tenant = "admin"
-     $admin_user =  "admin"
-     $admin_user_pass = "@dmin123"
-     $region = "regionOne"
-     notify {"Copy file for Keystone Cinder operations":} ->
-     file { '/tmp/keystone-cinder-operations.sh':
-            content => template('openstack-cinder/keystone-cinder-operations.sh'),
-            mode    => 0755,
-          } ->  
-
-      notify {"Keystone operations for Cinder":} ->
-      exec {"keystone operations for Cinder":
-            command => "bash /tmp/keystone-cinder-operations.sh",
-            user => 'root',
-      } 
+    notify {"Keystone operations for Cinder":} ->
+    exec {"keystone operations for Cinder":
+        command => "bash /tmp/keystone-cinder-operations.sh",
+        user => 'root',
+    } 
 }
