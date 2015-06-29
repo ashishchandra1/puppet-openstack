84552949fd3068336cb4b3503fd573e896e64bc8
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-06-29 15:49:05 +0400

    Update rabbitmq-ha.pp

--- modules/rabbitmq-ha/manifests/rabbitmq-ha.pp
+++ modules/rabbitmq-ha/manifests/rabbitmq-ha.pp
@@ -5,7 +5,8 @@ class rabbitmq-ha::rabbitmq-ha {
     exec {'rabbitmqctl stop_app':} ->
     exec {'rabbitmqctl join_cluster rabbit@controller1':} ->
     exec {'rabbitmqctl start_app':} ->
+    exec {'rabbitmqctl set_policy ha-all '^(?!amq\.).*' '{"ha-mode": "all"}'':} ->
     exec {'restart rabbitmq service on sec nodes':
          command => 'systemctl restart rabbitmq-server.service'
-         }
+    }
 }
