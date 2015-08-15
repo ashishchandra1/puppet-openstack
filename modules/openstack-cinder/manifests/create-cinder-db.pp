dde836676917cde79d4266a24e48f0609abadee0
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-08-15 12:59:48 +0400

    Modified Cinder DB Password

--- modules/openstack-cinder/manifests/create-cinder-db.pp
+++ modules/openstack-cinder/manifests/create-cinder-db.pp
@@ -1,9 +1,9 @@
 class openstack-cinder::create-cinder-db inherits openstack-cinder::params {
 
     notify {"Creating a new cinder database ":} ->
-    exec { "create-${db_name}-db":
-        unless => "/usr/bin/mysql -u${db_user} -p${db_password} ${db_name}",
-        command => "/usr/bin/mysql -uroot -p$mysql_password -e \"create database ${db_name}; grant all privileges on ${db_name}.* to ${db_user}@localhost identified by '$db_password'; grant all privileges on ${db_name}.* to ${db_user}@'%' identified by '$db_password';\"",
+    exec { "create-${CINDER_DB_NAME}-db":
+        unless => "/usr/bin/mysql -u${CINDER_DB_USER} -p${CINDER_DB_PASSWORD} ${CINDER_DB_NAME}",
+        command => "/usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -e \"create database ${CINDER_DB_NAME}; grant all privileges on ${CINDER_DB_NAME}.* to ${CINDER_DB_USER}@localhost identified by '$CINDER_DB_PASSWORD'; grant all privileges on ${CINDER_DB_NAME}.* to ${CINDER_DB_USER}@'%' identified by '$CINDER_DB_PASSWORD';\"",
     }
 
 
--- modules/openstack-cinder/manifests/params.pp
+++ modules/openstack-cinder/manifests/params.pp
@@ -1,6 +1,5 @@
 class openstack-cinder::params {
     
-    $CINDER_DB_PASSWORD = '@dmin123'
     $CONTROLLER_HNAME = "controller"
     $RABBIT_HOSTS = "controller1:5672,controller2:5672,controller3:5672"
     $MEMCACHED_SERVERS = "controller1:11211,controller2:11211,controller3:11211"
@@ -10,16 +9,16 @@ class openstack-cinder::params {
     $KEYSTONE_HOST = "controller"
     $KEYSTONE_ADMIN_PORT = '35357'
     $ADMIN_TOKEN = '@dmin123'
-     
+    
+    $MYSQL_ROOT_PASSWORD = '459nvDB91fWU'
+    $CINDER_DB_PASSWORD = 'jvTjSYt3BOFF'
+    $CINDER_DB_NAME = 'cinder'
+    $CINDER_DB_USER = 'cinder'
+ 
     $admin_tenant = "admin"
     $admin_user =  "admin"
     $admin_user_pass = "@dmin123"
     $region = "regionOne"
-
-    $mysql_password = '@dmin123'
-    $db_user = 'cinder'
-    $db_name = 'cinder'
-    $db_password = '@dmin123'
 
     $cinder_packages = [
         "openstack-cinder",
