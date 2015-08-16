c2e55618a536c604649a72bdf95708b77e5fe9b5
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-08-16 09:58:26 +0400

    Minor typos and other issues

--- modules/openstack-cinder/manifests/cinder-operations.pp
+++ modules/openstack-cinder/manifests/cinder-operations.pp
@@ -2,14 +2,14 @@ class openstack-cinder::cinder-operations {
 
  notify {"Enable Cinder Service":} ->
   exec {"Enabled Cinder Services":
-    command => 'systemctl enable openstack-cinder-api.service openstack-cinder-scheduler.service',
+    command => 'systemctl enable openstack-cinder-api.service openstack-cinder-scheduler.service openstack-cinder-volume.service',
     user => 'root',
     require =>  Package["python-cinderclient"],
   } ->
 
   notify {"Restart Cinder Services":} ->
   exec {"Restart Cinder Services":
-    command => 'systemctl restart openstack-cinder-api.service openstack-cinder-scheduler.service',
+    command => 'systemctl restart openstack-cinder-api.service openstack-cinder-scheduler.service openstack-cinder-volume.service',
     user => 'root',
     require =>  Package["python-cinderclient"],
   }
--- modules/openstack-neutron/manifests/neutron-db-sync.pp
+++ modules/openstack-neutron/manifests/neutron-db-sync.pp
@@ -1,7 +1,7 @@
 class openstack-neutron::neutron-db-sync inherits openstack-neutron::params {
 
     notify {"Syncing DB For Neutron":} ->
-    exec {"Nova DB Syncing":
+    exec {"Neutron DB Syncing":
         command => "su -s /bin/sh -c 'neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head' neutron",
         user => 'root',
     } 
