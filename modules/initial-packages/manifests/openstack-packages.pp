910f4f7b468e429c62da44268d7e7b14d6afd451
Ashish Chandra <mail.ashishchandra@gmail.com>
2016-06-20 08:42:09 +0400

    Added files for Initial packages for Openstack Mitaka release

--- modules/initial-packages/manifests/openstack-packages.pp
+++ modules/initial-packages/manifests/openstack-packages.pp
@@ -1,23 +1,18 @@
 class initial-packages::openstack-packages {
-    notify {"Installing epel-release ":} ->
-    package { "epel-release":
-        ensure => installed,
-        provider => rpm,
-        source => "http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm",
-        allow_virtual => false,
-    } ->
 
-    notify {"Installing RDO kilo release packages":} ->
-    package {"rdo-release-kilo":
+    notify {"Installing Openstack Mitaka Packages":} ->
+    package { 'centos-release-openstack-mitaka':
         ensure => installed,
-        provider => rpm,
-        source => "http://rdo.fedorapeople.org/openstack-kilo/rdo-release-kilo.rpm",
-        allow_virtual => false,
-    } -> 
+    } ->
 
     notify {"Upgrading installed packages":} ->
     exec {'yum -y upgrade':} ->
  
+    notify {"Install Python Openstack Client":} ->
+    package { 'python-openstackclient':
+        ensure => installed,
+    } ->
+
     notify {"Install openstack SELinux package":} ->
     package { 'openstack-selinux':
         ensure => installed,
