0dc5e3587ea78655c81ebb1d573cea39937c4581
Ashish Chandra <mail.ashishchandra@gmail.com>
2016-09-25 23:38:43 +0400

    Modified glance and nova modules, not tested yet

--- modules/openstack-nova/templates/nova-controller/keystone-nova-operations.sh
+++ modules/openstack-nova/templates/nova-controller/keystone-nova-operations.sh
@@ -7,8 +7,6 @@ admin_port="<%= @KEYSTONE_ADMIN_PORT %>"
 admin_token="<%= @ADMIN_TOKEN %>"
 region="<%= @region %>"
 
-admin_tenant="<%=  @admin_tenant %>"
-admin_user="<%= @admin_user %>"
 admin_user_pass="<%= @admin_user_pass %>"
 nova_user_pass="<%= @NOVA_USER_PASSWORD %>"
 
@@ -16,7 +14,7 @@ nova_user_pass="<%= @NOVA_USER_PASSWORD %>"
 export OS_PROJECT_DOMAIN_NAME=default
 export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=admin
-export OS_USERNAME=${admin_user}
+export OS_USERNAME=admin
 export OS_PASSWORD=${admin_user_pass}
 export OS_AUTH_URL=http://${keystone_host}:${admin_port}/v3
 export OS_IDENTITY_API_VERSION=3
@@ -34,7 +32,7 @@ if [ "$user_id" ]; then
         echo "Found existing user id: $user_id"
 else
         # Create the user
-        openstack user create --domain default nova --password="$nova_user_pass" --email="nova@example.com"
+        openstack user create --domain default nova --password="$nova_user_pass"
         
         # Add the admin role to nova user
         openstack role add --project service --user nova admin
@@ -84,13 +82,6 @@ endpoint_id=$(get_keystone_endpoint nova compute )
 if [ "$endpoint_id" ]; then
         echo "Found existing endpoint: $endpoint_id"
 else
- 
-   openstack endpoint create \
-            --publicurl http://"$keystone_host":8774/v2/%\(tenant_id\)s \
-            --internalurl http://"$keystone_host":8774/v2/%\(tenant_id\)s \
-            --adminurl http://"$keystone_host":8774/v2/%\(tenant_id\) \
-            --region "$region" \
-            compute
-fi            
-            
-
+   openstack endpoint create --region "$region" compute internal http://"$keystone_host":8774/v2.1/%\(tenant_id\)s
+   openstack endpoint create --region "$region" compute admin http://"$keystone_host":8774/v2.1/%\(tenant_id\)s           
+fi
