5fa32d5d0ef8f8505118cf715296ce70d9468b74
Ashish Chandra <mail.ashishchandra@gmail.com>
2017-03-14 21:27:54 +0300

    Added files for live migration, working fine

--- modules/openstack-nova/manifests/nova-compute/nova-compute-installation.pp
+++ modules/openstack-nova/manifests/nova-compute/nova-compute-installation.pp
@@ -14,6 +14,22 @@ class openstack-nova::nova-compute::nova-compute-installation inherits openstack
        content => template('openstack-nova/nova-compute/nova.conf.erb'),
    } ->
 
+   notify {"Editing libvirtd file for live migration":} ->
+   file { "/etc/sysconfig/libvirtd":
+       ensure  => file,
+       owner  => root,
+       group  => nova,
+       content => template('openstack-nova/nova-compute/libvirtd.erb'),
+   } ->
+
+   notify {"Editing libvirtd.conf file for live migration":} ->
+   file { "/etc/libvirt/libvirtd.conf":
+       ensure  => file,
+       owner  => root,
+       group  => nova,
+       content => template('openstack-nova/nova-compute/libvirtd.conf.erb'),
+   } ->
+   
    notify {"CREATING original nova.conf file for compute nodes":} ->
    file { "/etc/nova/nova.conf.orig":
        ensure  => file,
